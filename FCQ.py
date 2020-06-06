4c92f046feb5e601cee4503b006010de48a2308e
Julio Borges <11631138+jcborges@users.noreply.github.com>
2020-06-06 06:01:16 +0200

    Update FCQ.py

--- FCQ.py
+++ FCQ.py
@@ -5,12 +5,6 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64  # minibatch size
-GAMMA = 0.99  # discount factor
-TAU = 1e-3  # for soft update of target parameters
-LR = 5e-4  # learning rate
-UPDATE_EVERY = 4  # how often to update the network
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
